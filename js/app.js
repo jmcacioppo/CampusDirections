b001f27c7513c567a2d1f96760989fc122f632c5
jmcacioppo <jmcacioppo0531@gmail.com>
2018-01-31 10:03:01 -0500

    Angular-chat is functional

--- js/app.js
+++ js/app.js
@@ -1,26 +1,25 @@
 'use strict';
 
-var campusDirections = angular.module('campusDirections', ['ngRoute'])
-    .config(['$stateProvider', '$urlRouterProvider'], 
-    function($routeProvider, $stateProvider, $urlRouterProvider) {
-        // $routeProvider
-        //     .when('/', {
-        //         templateUrl: 'views/home.html',
-        //         controller: 'HomeController'
-        //     })
-        //     .otherwise({redirectTo: '/'});
-        $urlRouterProvider.otherwise('/home');
-
-        $stateProvider
-            .state('app', {
-                abstract: true,
-                template: '<ui-view></ui-view>'
-            })
-            .state('app.main', {
-                url: '/home',
+var campusDirections = angular.module('campusDirections', ['chat', 'ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
                 templateUrl: 'views/home.html',
                 controller: 'HomeController'
             })
+            .otherwise({redirectTo: '/'});
+        // $urlRouterProvider.otherwise('/home');
+
+        // $stateProvider
+        //     .state('app', {
+        //         abstract: true,
+        //         template: '<ui-view></ui-view>'
+        //     })
+        //     .state('app.main', {
+        //         url: '/home',
+        //         templateUrl: 'views/home.html',
+        //         controller: 'HomeController'
+        //     })
         
         // Use the HTML5 History API
         //$locationProvider.html5Mode(true);
--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,9 +1,43 @@
-'use strict';
-angular.module('campusDirections', ['chat'])
-    .controller('HomeController', HomeController);
+// 'use strict';
+// angular.module('campusDirections', ['chat'])
+//     .controller('HomeController', HomeController);
 
-HomeController.$inject = ['$scope', 'Messages'];
+// HomeController.$inject = ['$scope', 'Messages'];
 
-function HomeController($scope, Messages) {
-    $scope.name = "Joseph";
-}
+// function HomeController($scope, Messages) {
+//     $scope.name = "Joseph";
+// }
+
+// var chat = angular.module( 'campusDirections', ['chat'] );
+// chat.controller( 'HomeController', [ 'Messages', '$scope', function( Messages, $scope ) {
+//     // Message Inbox 
+//     $scope.messages = [];
+//     // Receive Messages 
+//     Messages.receive(function(message){
+//         $scope.messages.push(message);
+//     });
+//     // Send Messages 
+//     $scope.send = function() {
+//         Messages.send({ data : $scope.textbox });
+//     };
+// } ] );
+
+
+campusDirections.controller('HomeController', ['$scope', 'Messages',
+    function($scope, Messages) {
+        $scope.name = "Joseph";
+
+        // Message Inbox 
+        $scope.messages = [];
+
+        // Receive Messages 
+        Messages.receive(function(message){
+            $scope.messages.push(message);
+        });
+
+        // Send Messages 
+        $scope.send = function() {
+            Messages.send({ data : $scope.textbox });
+        };
+    }
+]);
