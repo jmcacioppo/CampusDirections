06415fba688d729bb6d44f6b74ca9397ac504e8c
jmcacioppo <jmcacioppo0531@gmail.com>
2018-01-31 12:10:22 -0500

    Put in basic parts of home page

--- js/app.js
+++ js/app.js
@@ -8,18 +8,6 @@ var campusDirections = angular.module('campusDirections', ['chat', 'ngRoute'])
                 controller: 'HomeController'
             })
             .otherwise({redirectTo: '/'});
-        // $urlRouterProvider.otherwise('/home');
-
-        // $stateProvider
-        //     .state('app', {
-        //         abstract: true,
-        //         template: '<ui-view></ui-view>'
-        //     })
-        //     .state('app.main', {
-        //         url: '/home',
-        //         templateUrl: 'views/home.html',
-        //         controller: 'HomeController'
-        //     })
         
         // Use the HTML5 History API
         //$locationProvider.html5Mode(true);
--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,43 +1,47 @@
-// 'use strict';
-// angular.module('campusDirections', ['chat'])
-//     .controller('HomeController', HomeController);
-
-// HomeController.$inject = ['$scope', 'Messages'];
-
-// function HomeController($scope, Messages) {
-//     $scope.name = "Joseph";
-// }
-
-// var chat = angular.module( 'campusDirections', ['chat'] );
-// chat.controller( 'HomeController', [ 'Messages', '$scope', function( Messages, $scope ) {
-//     // Message Inbox 
-//     $scope.messages = [];
-//     // Receive Messages 
-//     Messages.receive(function(message){
-//         $scope.messages.push(message);
-//     });
-//     // Send Messages 
-//     $scope.send = function() {
-//         Messages.send({ data : $scope.textbox });
-//     };
-// } ] );
-
-
 campusDirections.controller('HomeController', ['$scope', 'Messages',
     function($scope, Messages) {
-        $scope.name = "Joseph";
+        $scope.action = 'receive';
+        $scope.receiveClass = 'selectedButton';
+        $scope.giveClass = 'unselectedButton';
 
+        $scope.selectLanguage = function() {
+            console.log($scope.language);
+        }
+
+        $scope.selectAction = function(action) {
+            if(action === 'receive') $scope.action = 'receive';
+            else if(action === 'give') $scope.action = 'give';
+
+            checkClass();
+        }
+
+        function checkClass() {
+            if($scope.action === 'receive') {
+                $scope.receiveClass = 'selectedButton';
+                $scope.giveClass = 'unselectedButton';
+            }
+            else if($scope.action === 'give') {
+                $scope.receiveClass = 'unselectedButton';
+                $scope.giveClass = 'selectedButton';
+            }        
+        }
+        
         // Message Inbox 
-        $scope.messages = [];
+        // $scope.messages = [];
 
-        // Receive Messages 
-        Messages.receive(function(message){
-            $scope.messages.push(message);
-        });
+        // // User 
+        // Messages.user({ id: 12345, name : 'Joseph' });
 
-        // Send Messages 
-        $scope.send = function() {
-            Messages.send({ data : $scope.textbox });
-        };
+        // // Receive Messages 
+        // Messages.receive(function(message){
+        //     $scope.messages.push(message);
+        //     //message.remove();
+        // });
+
+        // // Send Messages 
+        // $scope.send = function() {
+        //     Messages.send({ data : $scope.textbox });
+        //     $scope.textbox = '';
+        // };
     }
 ]);
