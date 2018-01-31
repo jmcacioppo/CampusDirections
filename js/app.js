d4b03fe277694179063c04b19e292df5c2dfb6bb
jmcacioppo <jmcacioppo0531@gmail.com>
2018-01-31 12:17:37 -0500

    Set up messages view and controller. Got rid of #! in url

--- js/app.js
+++ js/app.js
@@ -1,14 +1,18 @@
 'use strict';
 
 var campusDirections = angular.module('campusDirections', ['chat', 'ngRoute'])
-    .config(function($routeProvider) {
+    .config(function($routeProvider, $locationProvider) {
         $routeProvider
             .when('/', {
                 templateUrl: 'views/home.html',
                 controller: 'HomeController'
             })
+            .when('/messages', {
+                templateUrl: 'views/messages.html',
+                controller: 'MessagesController'
+            })
             .otherwise({redirectTo: '/'});
         
         // Use the HTML5 History API
-        //$locationProvider.html5Mode(true);
+        $locationProvider.html5Mode(true);
     });
--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -25,23 +25,5 @@ campusDirections.controller('HomeController', ['$scope', 'Messages',
                 $scope.giveClass = 'selectedButton';
             }        
         }
-        
-        // Message Inbox 
-        // $scope.messages = [];
-
-        // // User 
-        // Messages.user({ id: 12345, name : 'Joseph' });
-
-        // // Receive Messages 
-        // Messages.receive(function(message){
-        //     $scope.messages.push(message);
-        //     //message.remove();
-        // });
-
-        // // Send Messages 
-        // $scope.send = function() {
-        //     Messages.send({ data : $scope.textbox });
-        //     $scope.textbox = '';
-        // };
     }
 ]);
