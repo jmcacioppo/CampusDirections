bd829cee838cdfd3934d30ac13b8763dbf817f17
jmcacioppo <jmcacioppo0531@gmail.com>
2018-02-02 19:58:42 -0500

    Translator api almost functional in app

--- js/controllers/MessagesController.js
+++ js/controllers/MessagesController.js
@@ -1,6 +1,36 @@
-campusDirections.controller('MessagesController', ['$scope', 'Messages', 'TransferData', 'Notification', '$location',
-    function($scope, Messages, TransferData, Notification, $location) {
+campusDirections.controller('MessagesController', ['$scope', 'Messages', 'TransferData', 'Notification', '$location', '$http', 'TRANSLATOR_SUBSCRIPTION',
+    function($scope, Messages, TransferData, Notification, $location, $http, TRANSLATOR_SUBSCRIPTION) {        
         $scope.user = TransferData.getUser();
+        var host = '';
+        var path = '';
+        
+        function setURL() {
+            let target = 'fr-fr';
+            let text = 'Hello';
+            let params = '?to=' + target + '&text=' + encodeURI(text);
+            
+            host = 'https://api.microsofttranslator.com';
+            path = '/V2/Http.svc/Translate' + params;
+        }
+
+        function TranslateText() {
+            var data = {
+                key : TRANSLATOR_SUBSCRIPTION.key,
+                host: host,
+                path: path
+            }
+            
+            $http.post('http://localhost:3000/api/translate', data)
+                .then( (response) => {
+                    console.log(response);
+                })
+                .catch( (err) => {
+                    console.log(err);
+                });
+        }
+        
+        setURL();
+        TranslateText();
 
         // Message Inbox 
         $scope.messages = [];
--- server.js
+++ server.js
@@ -1,8 +1,51 @@
 var express = require('express');
+let https = require ('https');
+var bodyParser = require('body-parser');  
 var path = require('path');
 var app = express();
 var rootPath = path.normalize(__dirname);
 
 app.use(express.static(rootPath));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());  
 app.listen(3000);
 console.log("Listening at localhost:3000...");
+
+app.post('/api/translate', function(req, res) {    
+    var host = req.body.host;
+    var path = req.body.path;
+    var key = req.body.key;
+    
+    let response_handler = function (response) {
+        let body = '';
+
+        response.on ('data', function (d) {
+            body += d;
+        });
+
+        response.on ('end', function () {
+            console.log (body);
+            res.json(body);
+        });
+
+        response.on ('error', function (e) {
+            console.log ('Error: ' + e.message);
+        });
+    };
+
+    let Translate = function () {
+        let request_params = {
+            method : 'GET',
+            hostname : host,
+            path : path,
+            headers : {
+                'Ocp-Apim-Subscription-Key' : key
+            }
+        };
+
+        let request = https.request (request_params, response_handler);
+        request.end ();
+    }
+
+    Translate ();
+});
